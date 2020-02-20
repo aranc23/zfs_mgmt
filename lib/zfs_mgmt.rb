647d1758ef79c973431cd7e9821cd268e62f1f7d
Aran Cox <aran-cox@uiowa.edu>
2020-02-20 08:00:04 -0600

    add zfsget def, and snapshot subcommand

--- lib/zfs_mgmt.rb
+++ lib/zfs_mgmt.rb
@@ -1,4 +1,5 @@
 require "zfs_mgmt/version"
+require 'pp'
 
 module ZfsMgmt
   def self.readsnaps()
@@ -9,5 +10,22 @@ module ZfsMgmt
       end
     end
     a
+  end
+  def self.zfsget(properties=['name'],types=['filesystem','volume'],fs)
+    results={}
+    com = ['zfs', 'get', '-Hp', properties.join(','), '-t', types.join(','), fs]
+    print(com,"\n")
+    %x| #{com.join(' ')} |.split("\n").each do |line|
+      params = line.split("\t")
+      unless results.has_key?(params[0])
+        results[params[0]] = {}
+      end
+      results[params[0]][params[1]] = params[2]
+      results[params[0]]["#{params[1]}@source"] = params[3]
+    end
+    return results
+  end
+  def self.snapshot_purge(noop=True)
+    pp self.zfsget(properties=['all'],types=['snapshot'],fs='')
   end
 end
