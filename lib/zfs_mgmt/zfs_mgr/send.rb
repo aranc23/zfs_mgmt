f5e8dc074f8c196600d1b2ae8d655419486c2e0a
Aran Cox <arancox@gmail.com>
2021-03-02 09:27:27 -0600

    again re-org cli building, add --holds and --drop-holds options

--- lib/zfs_mgmt/zfs_mgr/send.rb
+++ lib/zfs_mgmt/zfs_mgr/send.rb
@@ -23,6 +23,8 @@ class ZfsMgmt::ZfsMgr::Send < Thor
   method_option :mbuffer_size, :desc => "passed to mbuffer -s option", :type => :string
   method_option :exclude, :aliases => :'-x', :desc => "passed to -x option of receive side", :type => :array
   method_option :option, :aliases => :'-o', :desc => "passed to -o option of receive side", :type => :array
+  method_option :drop_holds, :desc => "pass the -h option to zfs recv, indicating holds should be ignored", :type => :boolean, :default => false
+  method_option :holds, :desc => "pass the -h option to zfs sned", :type => :boolean, :default => false
   def all()
     ZfsMgmt.global_options = options
 
